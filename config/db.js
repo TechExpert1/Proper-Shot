918b02e9aab6c61165ad48f7e61e412c47b37a18
Zeeshan Virk <48258523+TechExpert1@users.noreply.github.com>
2024-08-15 22:11:54 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/Proper_Shot");
+const connection = mongoose.connect("mongodb+srv://dookappmongo:rR6vlkRT62712K0x@cluster0.yat0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 connection.then(()=>{
     console.log("Database Connected Successfully !");
 }).catch((e)=>{
