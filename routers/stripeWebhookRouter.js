7549bb5b8201c429998c6bf34b282d87881ff487
shahmeer-ali <shahmeer@live.ca>
2025-07-25 12:41:47 +0500

    update

--- routers/stripeWebhookRouter.js
+++ routers/stripeWebhookRouter.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { stripeSubscriptionWebhook, createSubscription,cancelSubscription,confirmPayment } = require('../controllers/stripeWebHookController');
+const {
+    stripeSubscriptionWebhook,
+    createSubscription,
+    cancelSubscription,
+    confirmPayment,
+} = require('../controllers/stripeWebHookController');
 const authorizationMiddleware = require('../middlewares/myAuth');
 
 const stripeRouter = express.Router();
-stripeRouter.post(
-  "/webhook", 
-  stripeSubscriptionWebhook
-);
+stripeRouter.post('/webhook', stripeSubscriptionWebhook);
 // Create subscription route
 stripeRouter.post('/create-subscription', createSubscription);
-stripeRouter.post('/cancel',authorizationMiddleware,cancelSubscription)
-stripeRouter.post('/confirmpayment',authorizationMiddleware,confirmPayment)
+stripeRouter.post('/cancel', authorizationMiddleware, cancelSubscription);
+stripeRouter.post('/confirmpayment', authorizationMiddleware, confirmPayment);
 module.exports = stripeRouter;
